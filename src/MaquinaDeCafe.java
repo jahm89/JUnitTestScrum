c81667cb3a5cade4e6ab9e243fcb08efd419d2e5
Jeovanni Hernandez Morales <jeovanni.hernandez@telusinternational.com>
2018-09-28 12:08:09 -0600

    first commit

---
+++ src/MaquinaDeCafe.java
@@ -0,0 +1,93 @@
+
+public class MaquinaDeCafe {
+	private Cafetera cafetera;
+	private Vaso vasoPequeno;
+	private Vaso vasoMediano;
+	private Vaso vasoGrande;
+	private Azucarero azucar;
+	
+//	public MaquinaDeCafe(Cafetera cafe, Vaso vasoPequeno, Vaso vasoMediano, Vaso vasoGrande, Azucarero azucar) {
+//		super();
+//		this.cafe = cafe;
+//		this.vasoPequeno = vasoPequeno;
+//		this.vasoMediano = vasoMediano;
+//		this.vasoGrande = vasoGrande;
+//		this.azucar = azucar;
+//	}
+	
+	public Vaso getTipoVaso(String tipoDeVaso) {
+		
+		if(tipoDeVaso.equals("pequeno"))
+			return this.vasoPequeno;
+		else if(tipoDeVaso.equals("mediano"))
+			return this.vasoMediano;
+		else
+			return this.vasoGrande;
+		
+	}
+	
+	public String getVasoDeCafe(Vaso tipoDeVaso, int cantidadDeVasos, int cantidadDeAzucar) {
+		
+		if(tipoDeVaso.getCantidadDeVasos() <= cantidadDeVasos)
+			return "No hay vasos";
+		
+		int cantidadTotalCafe = tipoDeVaso.getContenido()* cantidadDeVasos;
+		
+		if(cantidadTotalCafe > this.cafetera.getCantidadDeCafe())
+			return "No hay cafe";
+		
+		if(cantidadDeAzucar > this.azucar.getCantidadDeAzucar())
+			return "No hay azucar";
+		
+		this.cafetera.setCantidadDeCafe(this.cafetera.getCantidadDeCafe() - cantidadTotalCafe);
+		tipoDeVaso.setCantidadVasos(tipoDeVaso.getCantidadDeVasos() - cantidadDeVasos);
+		this.azucar.setCantidadDeAzucar(this.getAzucar().getCantidadDeAzucar() - cantidadDeAzucar);
+		
+		return "Felicitaciones";
+		
+	}
+
+	public Cafetera getCafetera() {
+		return cafetera;
+	}
+
+	public void setCafetera(Cafetera cafetera) {
+		this.cafetera = cafetera;
+	}
+
+	public Vaso getVasoPequeno() {
+		return vasoPequeno;
+	}
+
+	public void setVasoPequeno(Vaso vasoPequeno) {
+		this.vasoPequeno = vasoPequeno;
+	}
+
+	public Vaso getVasoMediano() {
+		return vasoMediano;
+	}
+
+	public void setVasoMediano(Vaso vasoMediano) {
+		this.vasoMediano = vasoMediano;
+	}
+
+	public Vaso getVasoGrande() {
+		return vasoGrande;
+	}
+
+	public void setVasoGrande(Vaso vasoGrande) {
+		this.vasoGrande = vasoGrande;
+	}
+
+	public Azucarero getAzucar() {
+		return azucar;
+	}
+
+	public void setAzucar(Azucarero azucar) {
+		this.azucar = azucar;
+	}
+	
+	
+	
+	
+}
